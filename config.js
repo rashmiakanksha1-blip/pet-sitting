6fa34e81f55727bcbc905690826e8d977658fb45
Pet Sitters Club <petsittersclublondon@gmail.com>
2026-06-11 18:30:53 +0100

    Swap ribbon and book button colours (cf)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ← Edit these to customise your site
 const SITE_CONFIG = {
-  siteVersion: '10 Jun 2026 (ce)',
+  siteVersion: '10 Jun 2026 (cf)',
   businessName: 'Pet Sitters Club',
   bookButtonLabel: 'Book my slot',
   bookingInquirySubject: 'Pet sitting inquiry - Pet Sitters Club',
