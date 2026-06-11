c0a007847ba5f2f13c0011bf7ade172b1a7c3675
Pet Sitters Club <petsittersclublondon@gmail.com>
2026-06-11 01:34:30 +0100

    Light sage on availability header and legend ribbon

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ← Edit these to customise your site
 const SITE_CONFIG = {
-  siteVersion: '10 Jun 2026 (af)',
+  siteVersion: '10 Jun 2026 (ah)',
   businessName: 'Pet Sitters Club',
   bookingInquirySubject: 'Pet sitting inquiry - Pet Sitters Club',
   tagline: 'Sit, stay. We\'ll take it from here',
