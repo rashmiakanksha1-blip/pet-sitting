66226465bb9a15169febfb1ca9751f2badbb4ec3
Pet Sitters Club <petsittersclublondon@gmail.com>
2026-06-11 18:33:58 +0100

    White who-we-are ribbon, terracotta rate cards (cg)

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ← Edit these to customise your site
 const SITE_CONFIG = {
-  siteVersion: '10 Jun 2026 (cf)',
+  siteVersion: '10 Jun 2026 (cg)',
   businessName: 'Pet Sitters Club',
   bookButtonLabel: 'Book my slot',
   bookingInquirySubject: 'Pet sitting inquiry - Pet Sitters Club',
